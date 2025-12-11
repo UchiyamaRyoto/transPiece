2ebb448f06eb62b03e5a43f2540074379c4934ad
ryomaru <ryomaru.1109.g@gmail.com>
2025-12-11 12:53:54 +0900

    origin追加

--- laravel-backend/config/cors.php
+++ laravel-backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000','https://develop.d19hc3rqpf44ck.amplifyapp.com'],
 
     'allowed_origins_patterns' => [],
 
